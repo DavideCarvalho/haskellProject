263b6079d0568a8fe1ad13b2098de49f97873dcc
Wibbly <alexsander.gomes1996@gmail.com>
2016-11-25 16:17:43 +0000

    Atualizando Foundation com postgres e fazendo primeira tabela

--- haskellProject/Foundation.hs
+++ haskellProject/Foundation.hs
@@ -1,19 +1,39 @@
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TemplateHaskell   #-}
-{-# LANGUAGE TypeFamilies      #-}
-{-# LANGUAGE ViewPatterns      #-}
+{-# LANGUAGE OverloadedStrings, TypeFamilies, QuasiQuotes,
+             TemplateHaskell, GADTs, FlexibleContexts,
+             MultiParamTypeClasses, DeriveDataTypeable, EmptyDataDecls,
+             GeneralizedNewtypeDeriving, ViewPatterns, FlexibleInstances #-}
 module Foundation where
 
-import Yesod.Core
+import Yesod
+import Data.Text
+import Database.Persist.Postgresql
+    ( ConnectionPool, SqlBackend, runSqlPool)
 
-data App = App
-{-#
-data Estab = Estab
-    { nome  :: Text
-    , local :: Text
-    } deriving (Show)
-#-}
+data App = App {connPool :: ConnectionPool }
+
+share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
+    
+Usuarios json
+    nome         Text
+    rg           Text
+    senha        Text
+    email        Text
+
+|]
 
 mkYesodData "App" $(parseRoutesFile "routes")
 
 instance Yesod App
+
+instance YesodPersist App where
+   type YesodPersistBackend App = SqlBackend
+   runDB f = do
+       master <- getYesod
+       let pool = connPool master
+       runSqlPool f pool
+       
+       
+type Form a = Html -> MForm Handler (FormResult a, Widget)
+
+instance RenderMessage App FormMessage where
+    renderMessage _ _ = defaultFormMessage
