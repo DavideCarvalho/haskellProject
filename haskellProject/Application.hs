bc54b908d6abb7fc8bcfa8502e68bc80f1a0b647
Davi de Carvalho <Davi de Carvalho>
2016-11-26 13:49:15 -0200

    wai-cors adicionado para que seja possível fazer requisições cross-domain

--- haskellProject/Application.hs
+++ haskellProject/Application.hs
@@ -17,3 +17,14 @@ import Handlers.Login
 import Handlers.Reservas
 
 mkYesodDispatch "App" resourcesApp
+
+addCORStoWOFF :: W.Middleware
+addCORStoWOFF app = fmap updateHeaders . app
+  where
+    updateHeaders (W.ResponseFile    status headers fp mpart) = W.ResponseFile    status (new headers) fp mpart
+    updateHeaders (W.ResponseBuilder status headers builder)  = W.ResponseBuilder status (new headers) builder
+    updateHeaders (W.ResponseSource  status headers src)      = W.ResponseSource  status (new headers) src
+    new headers | woff      = cors : headers
+                | otherwise =        headers
+      where woff = lookup HT.hContentType headers == Just "application/font-woff"
+            cors = ("Access-Control-Allow-Origin", "*")
